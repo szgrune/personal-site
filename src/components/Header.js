f1fa17ac691b00d640c043e0ad05b937ceabdd1c
szgrune <samzgrunebaum@gmail.com>
2023-11-01 00:40:51 -0400

    header margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ export default function Header(props) {
     };
 
     return (
-      <div className="header" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: '3vh', marginLeft: '5vw', marginRight: '5vw' }}>
+      <div className="header" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: '3vh', marginLeft: '5vw', marginRight: '5vw', marginBottom: '5vw' }}>
         <Typography variant="h5" style={{ cursor: "pointer" }} button onClick={() => redirectRoute("/")}>samuel z grunebaum</Typography>
         <Box classname="menuDesktop" sx={{ display: {xs:"none", sm: "flex" }, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
           <Typography variant="h6" style={{ cursor: "pointer", marginRight: "3vw" }} button onClick={() => redirectRoute("/about")}>about</Typography>
