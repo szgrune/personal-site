fa8200bb9f6ae73a92a5aa88c155849989592a5e
szgrune <samzgrunebaum@gmail.com>
2024-06-24 18:34:30 -0400

    tagline

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,8 +30,8 @@ export default function Home() {
 
     return (
     <div>
-        <Typography variant="h2" component="h2" sx={{ textAlign: 'center' }}>
-            Designing experiences that work for every customer
+        <Typography variant="h3" component="h3" sx={{ textAlign: 'center', marginBottom: '5vh' }}>
+            Designing experiences to serve every customer
         </Typography>
         <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'left' , marginLeft: '5vw', marginRight: '5vw'}}>
             {/* rendering the card component with card content */}
